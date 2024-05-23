26da6d38330eeda62ef49bf475107ecbae06ecbf
MatarSherman <matarsherman1@email.com>
2024-05-23 22:20:21 +0300

    format

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
-
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    CONNECTION_STRING: str = ''
+    CONNECTION_STRING: str = ""
 
     class Config:
-        env_file = '.env'
+        env_file = ".env"
+
 
 @lru_cache
 def get_settings() -> Settings:
