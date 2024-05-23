27d8098d7e91038325020e3f23a6850d62e9f6e7
MatarSherman <matarsherman1@email.com>
2024-05-23 12:46:22 +0300

    utilize Counter most_common in get_sorted_tlds_amounts

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,7 @@ from collections import Counter
 from tld import get_tld
 
 
-def get_sorted_tlds_amounts(urls: list[str]) -> dict[str, int]:
+def get_sorted_tlds_amounts(urls: set[str]) -> dict[str, int]:
     tlds = (get_tld(url, fail_silently=True) for url in urls)
     tlds_counts = Counter((tld for tld in tlds if tld))
-    return dict(
-        sorted(
-            tlds_counts.items(),
-            key=lambda countEntry: countEntry[1],
-            reverse=True
-        )
-    )
+    return dict(tlds_counts.most_common())
